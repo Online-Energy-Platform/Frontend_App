870142d4de9cdeea373973582810e5a3f269e3e6
Alexandra-Pop <100316034+Alexandra-Pop@users.noreply.github.com>
2023-01-17 11:10:05 +0200

    Add files via upload

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -1,4 +1,4 @@
 export const HOST = {
-    //backend_api: 'http://localhost:8080',
-	backend_api: 'http://20.113.114.57:8080',
+    // backend_api: 'http://localhost:8080',
+	backend_api: 'http://20.113.44.6:8080',
 };
